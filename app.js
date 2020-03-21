8dc01d84e3a8a20e96de2e0e30525050a9233bd6
Harsh <harshmacc@gmail.com>
2020-03-21 14:27:21 +0530

    created a js obj named post

--- app.js
+++ app.js
@@ -39,7 +39,10 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req , res){
-   console.log(req.body.compose);
+   const post = {
+    title : req.body.postTitle,
+    content : req.body.postBody
+   };
 });
 
 
